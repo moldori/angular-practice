733d1c54f9f15a600fdb52d2466a8f0ad1a699a1
moldori <molnaardora@gmail.com>
2020-01-22 13:46:08 +0100

    upload first files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,59 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'task-manager';
+
+  name : string = 'Dora';
+  id : number = 1 ;
+  isImportant : boolean = true;
+
+  greeter() {
+    console.log('Hello !')
+  }
+
+
+  num1 : number;
+  num2 : number;
+  res : number;
+  sol : string;
+
+  sum() {
+    this.res = +this.num1 + +this.num2;
+    console.log(this.res)
+    if(this.res >= 1000) {
+      console.log("Big number");
+      return "Big number";
+    } else if(this.res < 1000){
+      console.log("Small number")
+      return "Small number" ;
+    } else {
+      return "";
+    }
+  }
+
+  public arr = [];
+
+  namee : string;
+  age : number;
+  gender : string;
+
+  genders: string[] = ['Male', 'Female', 'Other'];
+
+  person = {name: "", age: 0, gender: ""};
+
+  counter : number = -1;
+
+  add() {
+    this.person.name = this.namee;
+    this.person.age = this.age;
+    this.person.gender = this.gender;
+
+    this.counter++;
+
+    this.arr[this.counter] = this.person;
+  
+    
+
+    console.log(this.arr);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatButtonModule} from '@angular/material/button';
+
+import { FormsModule } from '@angular/forms';
+import {MatRadioModule} from '@angular/material/radio';
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    FormsModule,
+    MatRadioModule
   ],
   providers: [],
   bootstrap: [AppComponent]
